2ac646bfe2248978ac47559378c620fb8fc712ad
Rhomagn <rsaintp@udel.edu>
2025-03-02 04:55:28 -0500

    Update script.js
    Boarders & health shit

--- code/script.js
+++ code/script.js
@@ -1,177 +1,178 @@
 let map;
 let savedMarkers = [];
-let currentBuildingMarker = null;  // Variable to store the current marker for the building
-let building = ''; // Variable to store the building name
-let Events = ''; // Variable to store the events in the building
+let currentBuildingMarker = null;  
+let building = ''; 
+let Events = '';  
 
-//EVENTS FOR BUILDINGS
+// === BUILDING EVENTS ===
 const buildingEvents = {
     "Alison Hall": [],
     "Alison Hall West": [],
     "Brown Lab": [],
     "Bob Carpenter Center": [],
     "Colburn Lab": [],
-    "Carpenter Sports Building": [['ELI Basketball Shootaround','<br>&nbsp; &nbsp; 3/6/25','2-3:30pm'],  ['Top Hen Challenge - 500m row',' <br>&nbsp; &nbsp; 3/10/25','6am-11pm']],
+    "Carpenter Sports Building": [
+        ['ELI Basketball Shootaround', '<br>&nbsp; &nbsp; 3/6/25', '2-3:30pm'],  
+        ['Top Hen Challenge - 500m row', '<br>&nbsp; &nbsp; 3/10/25', '6am-11pm']
+    ],
     "Drake Hall": [],
-    "Ewing": [],
     "DuPont Hall": [],
-    "Evans Hall": [["Microelectriconics Community Science Days","<br>&nbsp;&nbsp; 3/22/25","9:15am-12:30pm"]],
-    "Ewing Hall": [],
-    "Gore Hall": [['Heaven Can You Hear Me?','<br>&nbsp; &nbsp; 3/3/25','7pm-9pm'],["Prohibited Substances","<br>&nbsp; &nbsp; 3/6/25","12:40-1:35pm"]],
-    "Harker ISE Lab": [],
-    "Hullihen Hall": [],
-    "Kirkbride Lecture Hall": [],
-    "Lerner Hall": [],
-    "McDowell Hall": [],
-    "Memorial Hall": [['Gobsmacked!','<br>&nbsp; &nbsp; 3/4/25','5:30pm-6:45pm'],['Documentary Poetry','<br>&nbsp; &nbsp; 3/13/25','4pm-6pm']],
-    "Mitchell Hall": [],
-    "Morris Library": ["Honestly Abe","Transcendent Resilience"],
-    "Old College": [["Colors of Old College",' <br>&nbsp; &nbsp; 3/2/25','']],
-    "Penny Hall": ["Minerals from China and India","Scupltural Cooper Saved from the Smelter"],
-    "Pearson Hall": [],
-    "Purnell Hall": [],
-    "Perkins": [["Open Arcade", "<br>&nbsp; &nbsp;  3/3/25 - 3/4/25 ","12-11pm"], ["Geek Week"," <br>&nbsp; &nbsp;3/4/25","7pm"], ["Movie: Star Trek", " <br>&nbsp; &nbsp;3/5/25", "8:30-10:30pm"]],
-    "Purnell Hall": [],
-    "Recitation Hall": [["Colors, Land, and Building"," <br>&nbsp; &nbsp;3/2/25",""]],
-    "Robinson Hall": [],
-    "Sharp Lab": [["CIS Seminar: Wenjing Lou, Ph.D."," <br>&nbsp; &nbsp; 3/14/25","11:30am"],["CIS Seminar: Adam Perer, Ph.D."," <br>&nbsp; &nbsp; 3/21/25","11:30am"]],
-    "Smith Hall": [],
-    "Spencer Lab": [],
-    "Townsend Hall": [],
-    "Trabant": [["An Evening with John Cho"," <br>&nbsp; &nbsp; 3/3/25","5pm"],["Lego Fest"," <br>&nbsp; &nbsp; 3/4/25", "11am to 2pm"],["Band of the Bands","<br>&nbsp; &nbsp; 3/8/25","10:00pm-1:00am"]],
-    "Willard Hall": [],
-    "Wolf Hall": []
+    "Evans Hall": [["Microelectronics Community Science Days", "<br>&nbsp;&nbsp; 3/22/25", "9:15am-12:30pm"]],
+    "Gore Hall": [
+        ['Heaven Can You Hear Me?', '<br>&nbsp; &nbsp; 3/3/25', '7pm-9pm'],
+        ["Prohibited Substances", "<br>&nbsp; &nbsp; 3/6/25", "12:40-1:35pm"]
+    ],
+    "Memorial Hall": [
+        ['Gobsmacked!', '<br>&nbsp; &nbsp; 3/4/25', '5:30pm-6:45pm'],
+        ['Documentary Poetry', '<br>&nbsp; &nbsp; 3/13/25', '4pm-6pm']
+    ],
+    "Morris Library": ["Honestly Abe", "Transcendent Resilience"],
+    "Old College": [["Colors of Old College", '<br>&nbsp; &nbsp; 3/2/25', '']],
+    "Penny Hall": ["Minerals from China and India", "Sculptural Copper Saved from the Smelter"],
+    "Perkins": [
+        ["Open Arcade", "<br>&nbsp; &nbsp;  3/3/25 - 3/4/25 ", "12-11pm"],
+        ["Geek Week", "<br>&nbsp; &nbsp;3/4/25", "7pm"],
+        ["Movie: Star Trek", "<br>&nbsp; &nbsp;3/5/25", "8:30-10:30pm"]
+    ],
+    "Recitation Hall": [["Colors, Land, and Building", "<br>&nbsp; &nbsp;3/2/25", ""]],
+    "Sharp Lab": [
+        ["CIS Seminar: Wenjing Lou, Ph.D.", "<br>&nbsp; &nbsp; 3/14/25", "11:30am"],
+        ["CIS Seminar: Adam Perer, Ph.D.", "<br>&nbsp; &nbsp; 3/21/25", "11:30am"]
+    ],
+    "Trabant": [
+        ["An Evening with John Cho", "<br>&nbsp; &nbsp; 3/3/25", "5pm"],
+        ["Lego Fest", "<br>&nbsp; &nbsp; 3/4/25", "11am-2pm"],
+        ["Battle of the Bands", "<br>&nbsp; &nbsp; 3/8/25", "10:00pm-1:00am"]
+    ],
 };
 
-//Building events
+// === FUNCTION: DISPLAY BUILDING EVENTS ===
 function buildingEvent(currentBuilding) {
-    let eventList = buildingEvents[currentBuilding];
+    let eventList = buildingEvents[currentBuilding] || [];
     Events = `Events in: ${currentBuilding}`;
 
-    let eventDetails = '';
+    let eventDetails = eventList.length === 0
+        ? '<li>No events available</li>'
+        : eventList.map(event => Array.isArray(event) 
+            ? `<li>${event[0]} - ${event[1]} (${event[2]})</li>` 
+            : `<li>${event}</li>`).join('');
 
-    if (!eventList || eventList.length === 0) {
-        eventDetails = '<li>No events available</li>';
-    } else if (Array.isArray(eventList[0])) {
-        eventDetails = eventList.map(event => `<li>${event[0]} - ${event[1]} (${event[2]})</li>`).join('');
-    } else {
-        eventDetails = eventList.map(event => `<li>${event}</li>`).join('');
-    }
-
-    // Update the events-tab content
     const eventsTab = document.getElementById("events-tab");
     if (eventsTab) {
-        eventsTab.innerHTML = `
-            <h2>${Events}</h2>
-            <ul>${eventDetails}</ul>`;
+        eventsTab.innerHTML = `<h2>${Events}</h2><ul>${eventDetails}</ul>`;
     } else {
         console.error("events-tab element not found!");
     }
 }
 
+// === FUNCTION: INITIALIZE GOOGLE MAP ===
 function initMap() {
-    console.log("Initializing map...");
-    const universityLocation = { lat: 39.678, lng: -75.7526 };
-    
+    const udBounds = {
+        north: 39.6915,
+        south: 39.6600,
+        east: -75.7410,
+        west: -75.7700
+    };
+    const udCenter = { lat: 39.6766, lng: -75.7500 };
+
     map = new google.maps.Map(document.getElementById("map"), {
+        center: udCenter,
         zoom: 15,
-        center: universityLocation
+        restriction: { latLngBounds: udBounds, strictBounds: true },
+        mapTypeId: "roadmap"
     });
 
-    new google.maps.Marker({
-        position: universityLocation,
-        map: map,
-        title: "University of Delaware"
-    });
+    new google.maps.Marker({ position: udCenter, map, title: "University of Delaware" });
 
-    map.addListener("click", (event) => {
-        addSavedMarker(event.latLng);
-    });
+    google.maps.event.addListener(map, 'dragend', checkBounds);
 }
 
-// Function to clear the previous marker and add a new one for the selected building
-function visitBuilding(buildingName, location) {
-    // Remove the previous marker
-    if (currentBuildingMarker) {
-        currentBuildingMarker.setMap(null);
+// === FUNCTION: KEEP MAP WITHIN UD BOUNDARIES ===
+function checkBounds() {
+    const bounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(39.6600, -75.7700),
+        new google.maps.LatLng(39.6915, -75.7410)
+    );
+    if (!bounds.contains(map.getCenter())) {
+        map.setCenter({ lat: 39.6766, lng: -75.7500 });
     }
+}
 
-    // Create a new marker for the selected building
+// === FUNCTION: VISIT A BUILDING (SET MARKER & DISPLAY EVENTS) ===
+function visitBuilding(buildingName, location) {
+    if (currentBuildingMarker) currentBuildingMarker.setMap(null);
+
     currentBuildingMarker = new google.maps.Marker({
         position: location,
         map: map,
         title: buildingName
     });
 
-    // Center the map on the selected building and zoom in
     map.setCenter(location);
     map.setZoom(18);
-
-    // Update the events list
     buildingEvent(buildingName);
 }
 
+// === BUILDINGS DATA ===
 const buildings = {
-    "Alison Hall": { lat: 39.67873805117535, lng: -75.75062836166603 },
-    "Alison Hall West": { lat: 39.678524270217046, lng: -75.75116067637826 },
-    "Brown Lab": { lat: 39.67970143991899, lng:  -75.75133018566285 },
-    "Colburn Lab": { lat: 39.680160289848985, lng: -75.74993764638631 },
-    "Carpenter Sports Building": { lat: 39.68429061337597, lng: -75.7518218521466 },
-    "Drake Hall": { lat: 39.679754120901855, lng: -75.75072941951787 },
-    "DuPont Hall": { lat: 39.68071562726429, lng: -75.7515826529153},
-    "Evans Hall": { lat: 39.680185829739614, lng: -75.7512977119051 },
-    "Ewing Hall": { lat: 39.68138370641468, lng: -75.75501435936988 },
-    "Gore Hall": { lat: 39.680602266858706, lng: -75.7530588540025 },
-    "Harker ISE Lab": { lat: 39.67895603910478, lng: -75.748446857146 },
-    "Hullihen Hall": { lat: 39.679597969705306, lng:  -75.75289882396227 },
-    "Kirkbride Lecture Hall": { lat: 39.6812755519956, lng: -75.75434771907531 },
-    "Lerner Hall": { lat: 39.68066674209665, lng: -75.7558414934743 },
-    "McDowell Hall": { lat: 39.68392816012537, lng: -75.75457001813923 },
-    "Memorial Hall": { lat: 39.67918684703373, lng: -75.75214839554782 },
-    "Mitchell Hall": { lat: 39.68009262514897, lng: -75.75319006096672 },
-    "Morris Library": { lat: 39.678038629437935, lng: -75.75310455767755 },
-    "Old College": { lat: 39.6839757434548, lng: -75.75368788207594 },
-    "Penny Hall": { lat: 39.67828513028564, lng: -75.74930709520285 },
-    "Pearson Hall": { lat: 39.68016750626128, lng: -75.74865029635602 },
-    "Perkins": { lat: 39.676941615090975, lng: -75.7495330731378 },
-    "Purnell Hall": { lat: 39.680421311124974, lng: -75.75530652168841 },
-    "Recitation Hall": { lat: 39.68354843420127, lng: -75.75325275713652 },
-    "Robinson Hall": { lat: 39.6762761956673, lng: -75.75246961768218 },
-    "Sharp Lab": { lat: 39.681137291845275, lng:  -75.75288645136263 },
-    "Smith Hall": { lat: 39.6806318427531, lng: -75.75418663366268 },
-    "Spencer Lab": { lat: 39.680971011564296, lng: -75.74996854760275 },
-    "Townsend Hall": { lat: 39.66585668248938, lng: -75.75143344426971 },
-    "Trabant": { lat: 39.682381905336165, lng: -75.7542119350803 },
-    "Willard Hall": { lat: 39.68348011118033, lng: -75.75514733368128 },
-    "Wolf Hall": { lat: 39.681224696526826, lng: -75.75172575610068 }
+    "Alison Hall": { lat: 39.678738, lng: -75.750628 },
+    "Brown Lab": { lat: 39.679701, lng: -75.751330 },
+    "Carpenter Sports Building": { lat: 39.684290, lng: -75.751821 },
+    "Drake Hall": { lat: 39.679754, lng: -75.750729 },
+    "Gore Hall": { lat: 39.680602, lng: -75.753058 },
+    "Memorial Hall": { lat: 39.679186, lng: -75.752148 },
+    "Morris Library": { lat: 39.678038, lng: -75.753104 },
+    "Old College": { lat: 39.683975, lng: -75.753687 },
+    "Perkins": { lat: 39.676941, lng: -75.749533 },
+    "Sharp Lab": { lat: 39.681137, lng: -75.752886 },
+    "Trabant": { lat: 39.682381, lng: -75.754211 },
+    "Wolf Hall": { lat: 39.681224, lng: -75.751725 }
 };
 
-// Update the dropdown options with a click event for each building
+// === FUNCTION: POPULATE BUILDING DROPDOWN ===
 function addDropdownOptions() {
     const dropdown = document.getElementById("dropdown-content");
-    dropdown.innerHTML = "";  // Clear previous options
+    dropdown.innerHTML = "";
 
     Object.keys(buildings).forEach(building => {
         const option = document.createElement("a");
         option.href = "#";
         option.innerText = building;
         option.onclick = (event) => {
-            event.preventDefault();  // Prevent page jump
-            visitBuilding(building, buildings[building]);  // Call visitBuilding
+            event.preventDefault();
+            visitBuilding(building, buildings[building]);
         };
         dropdown.appendChild(option);
     });
 }
 
-// Function to center the map to the University of Delaware
+// === FUNCTION: CENTER MAP TO UD CAMPUS ===
 function centerMap() {
-    const universityLocation = { lat: 39.678, lng: -75.7526 };
-    map.setCenter(universityLocation);
+    const udLocation = { lat: 39.678, lng: -75.7526 };
+    map.setCenter(udLocation);
     map.setZoom(15);
 }
 
-// Call this function after the page loads
+// === INITIALIZE EVERYTHING ON PAGE LOAD ===
 document.addEventListener("DOMContentLoaded", () => {
     addDropdownOptions();
     initMap();
+});
+
+// Coordinates for UD Health Services
+const udHealthServices = { lat: 39.6784, lng: -75.7502 };
+
+document.getElementById("student-health").addEventListener("click", function(event) {
+    event.preventDefault();  // Prevent default behavior of the link
+
+    // Center the map on UD Health Services location
+    map.setCenter(udHealthServices);
+    map.setZoom(18);  // Adjust zoom level if necessary
+
+    // Optionally, add a marker at the UD Health Services location
+    new google.maps.Marker({
+        position: udHealthServices,
+        map: map,
+        title: "UD Health Services"
+    });
 });
